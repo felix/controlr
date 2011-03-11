dd1bd7d9bdae7df7c73dc184285062c2b6b949de
felix <felix@seconddrawer.com.au>
2011-03-11 14:42:08 +0700

    more styling and locales

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
 
+# create user
 admin = User.create(:email => 'root@example.com',
                     :firstname => 'Admin',
                     :surname => 'Istrator',
@@ -14,24 +8,19 @@ admin = User.create(:email => 'root@example.com',
                     :active => true,
                     :created_at => Time.now)
 
-user_perm = Permission.create(
-  :action => 'manage',
-  :subject_class => 'User'
-)
+# create permissions
+user_perm = Permission.create(:action => 'manage', :subject_class => 'User')
 
-super_role = Role.create(
-  :name => 'super',
-  :description => 'Super user'
-)
-admin_role = Role.create(
-  :name => 'admin',
-  :description => 'Admin user'
-)
+# create roles
+super_role = Role.create(:name => 'super', :description => 'Super user')
+admin_role = Role.create(:name => 'admin', :description => 'Admin user')
+
+# assign permissions
 super_role.permissions << user_perm
 admin_role.permissions << user_perm
 super_role.save
 admin_role.save
 
+# assign roles
 admin.roles << super_role
 admin.save
-
