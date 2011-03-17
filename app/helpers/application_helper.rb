b0c3e452368b23223fee089ece8ee89598f3f5b1
felix <felix@seconddrawer.com.au>
2011-03-17 13:05:15 +0700

    tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def create_tab(text,path)
+    klass = request.fullpath.include?(path) ? 'active' : ''
+    "<li class=#{klass}>#{link_to(text,path)}</li>".html_safe
+  end
 end
