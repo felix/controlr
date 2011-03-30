a8bcbe780e982bdbb3c77cef54070fa3d9a340f0
felix <felix@seconddrawer.com.au>
2011-03-30 14:48:35 +0700

    mailer

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require 'action_controller/railtie'
 require 'dm-rails/railtie'
-# require 'action_mailer/railtie'
+require 'action_mailer/railtie'
 # require 'active_resource/railtie'
 require 'rails/test_unit/railtie'
 
