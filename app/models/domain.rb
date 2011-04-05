d86f4db40279f3891cd1e0195f118115f1fc54ea
felix <felix@seconddrawer.com.au>
2011-04-05 16:17:57 +0700

    dns_active

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -12,6 +12,7 @@ class Domain
   property :email_max_quota, Integer, :default => '200000'
   property :ftp_active, Boolean
   property :ftp_quota, Integer, :default => 0
+  property :dns_active, Boolean, :default => 0
   property :passhash, String, :length => 32
   property :created_at, DateTime
   property :updated_at, DateTime
